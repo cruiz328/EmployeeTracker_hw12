5a9dfdf33c69d54be26279840cf01379765feb0e
cruiz328 <lemmy125@gmail.com>
2022-05-21 11:52:28 -0700

    update 2

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const inquirer = require("inquirer")
+const mysql = require("mysql2")
 
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: 'localhost',
+    host: "localhost",
     // MySQL username,
-    user: 'root',
+    user: "root",
     // {TODO: Add your MySQL password}
-    password: 'password',
-    database: 'employee_tracker'
+    password: "password",
+    database: "employee_tracker",
   },
   console.log(`Connected to the employee_tracker database.`)
-);
+)
 
 function start() {
-inquirer
-  .prompt([
-    // {
-    //   type: 'input',
-    //   name: 'name',
-    //   message: 'What is your name?'
-    // },
-    {
-      type: 'list',
-      message: 'Which do you want to see?',
-      name: 'View',
-      choices: ['View all departments', 'View all roles']
-    },
-  ])
+  inquirer
+    .prompt([
+      // {
+      //   type: 'input',
+      //   name: 'name',
+      //   message: 'What is your name?'
+      // },
+      {
+        type: "list",
+        message: "Which do you want to see?",
+        name: "View",
+        choices: ["View all departments", "View all roles"],
+      },
+    ])
 
-  .then(data => {
-    if (data.View == 'View all departments') {
-        db.query('SELECT * FROM department', function (err, results) {
-            console.log(results);
-            start()
-          });
-    }
-  });
+    .then((data) => {
+      if (data.View == "View all departments") {
+        db.query("SELECT * FROM department", function (err, results) {
+          console.log(results)
+          start()
+        })
+      } else if (data.View == "View all roles") {
+        db.query("SELECT * FROM role", function (err, results) {
+          console.log(results)
+          start()
+        })
+      }
+    })
 }
 
 start()
